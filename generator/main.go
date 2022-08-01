42846a338936d66ee1efbf37e137edc983156578
Augustas Vilcinskas <augustas.vilcinskas@ivido.nl>
2022-08-01 12:09:53 +0200

    change package name

--- generator/main.go
+++ generator/main.go
@@ -4,7 +4,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/ivido/go-fhir-stu3/generator/cmd"
+import "github.com/ivido/go-fhir-library/generator/cmd"
 
 func main() {
 	cmd.Execute()
